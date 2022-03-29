7f13afaf37b38548e3f18b87365ea8acc2d8c435
lx470100series <102318654+lx470100series@users.noreply.github.com>
2022-03-29 05:16:39 +0000

    Added to flask app

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ app = Flask('app')
 
 @app.route('/')
 def hello_world():
-  return 'Hello, World I love programming!'
+  return 'Hello, World I love programming, and so should you!!'
 
 app.run(host='0.0.0.0', port=8080)
